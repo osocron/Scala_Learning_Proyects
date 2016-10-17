e4fdb76ff8b0b80a8c0ce6caaa4399b24d62e70c
osocron <osocron@gmail.com>
2016-10-17 12:47:08 -0500

    added answers to life...

--- ScalaLearning/src/main/scala/fpinscala/parsers/Parsers.scala
+++
@@ -1,9 +0,0 @@
-package fpinscala.parsers
-
-/**
-  * Created by osocron on 10/17/16.
-  */
-trait Parsers[ParseError, Parser[_]] {
-  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def char(c: Char): Parser[Char]
-}
---
+++ ScalaLearning/src/test/scala/fpinscala/monoids/Monoid$Test.scala
@@ -0,0 +1,51 @@
+package fpinscala.monoids
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by osocron on 10/17/16.
+  */
+class Monoid$Test extends FunSuite {
+
+  import Monoid._
+
+  test("testIntAddition op") {
+    assert(intAddition.op(1, 2) == 3)
+  }
+
+  test("testIntAddition zero") {
+    assert(intAddition.op(intAddition.zero, 1) == 1)
+  }
+
+  test("testBooleanOr op") {
+    assert(booleanOr.op(true, false))
+  }
+
+  test("testBooleanOr zero") {
+    assert(booleanOr.op(booleanOr.zero, true))
+  }
+
+  test("testListMonoid op") {
+    assert(
+      listMonoid.op(List(1), List(2)) == List(1, 2))
+  }
+
+  test("testListMonoid zero") {
+    assert(
+      listMonoid.op(
+        listMonoid.zero, List(1)) == List(1))
+  }
+
+  test("testStringMonoid") {
+
+  }
+
+  test("testBooleanAnd") {
+
+  }
+
+  test("testIntMultiplication") {
+
+  }
+
+}
